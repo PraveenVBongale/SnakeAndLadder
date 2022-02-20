1ff4ae5973dda137ba066cc4c087d4df428226fd
PraveenVBongale <praveen.bongale98@gmail.com>
2022-02-20 21:14:16 +0530

    Total Dice Added

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderGame
+{
+    internal class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO SNAKE & LADDER PLAY");
+            SnakeandLadder snake = new SnakeandLadder();
+            snake.StartsGame();
+            snake.RandomNumber();
+            snake.TillWin();
+            snake.Win();
+            Console.ReadLine();
+        }
+    }
+}
